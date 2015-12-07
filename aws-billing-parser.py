b63c61eeaf720b943a04d9a7581d3e4e567e4949
Rafael Koike <koiker@amazon.com>
2015-12-07 17:45:17 -0200

    Just updated the python binary path

--- aws-billing-parser.py
+++ aws-billing-parser.py
@@ -1,4 +1,4 @@
-#!/usr/local/aws/bin/python
+#!/usr/bin/python
 # Copyright 2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You
